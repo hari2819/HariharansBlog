ac87fc330e8f852e59a936d604cfbc5b573ce4c9
Hariharan Vengatesan <108681854+hari2819@users.noreply.github.com>
2025-01-13 14:34:13 +0530

    Add files via upload

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HariharanBlog.Models;
+
+namespace HariharanBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private BlogContext db = new BlogContext();
+
+        public ActionResult Index()
+        {
+            var posts = db.BlogPosts.ToList();
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var post = db.BlogPosts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+	}
+}
---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HariharanBlog.Models;
+
+namespace HariharanBlog.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private BlogContext db = new BlogContext();
+        public ActionResult Index()
+        {
+            var projects = db.Projects.ToList();
+            return View(projects);
+
+        }
+	}
+}
